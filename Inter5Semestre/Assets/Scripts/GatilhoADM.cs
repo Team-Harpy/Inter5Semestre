a8fada0bb34c3e5bb617fba2ea3699d1565e0b8e
Vini Shoji <viniciusasato@gmail.com>
2021-04-18 18:26:33 -0300

    Activate Obstacles during hunt

--- Inter5Semestre/Assets/Scripts/GatilhoADM.cs
+++ Inter5Semestre/Assets/Scripts/GatilhoADM.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 public class GatilhoADM : Interactable
 {
     public GameObject monstro;
+    public GameObject obstaculo1;
+    public GameObject obstaculo2;
+    public GameObject obstaculo3;
 
     public override void Interact()
     {
         if (!monstro.activeInHierarchy)
+        {
             monstro.SetActive(true);
+            obstaculo1.SetActive(true);
+            obstaculo2.SetActive(true);
+            obstaculo3.SetActive(true);
+        }
+         
         else if (monstro.activeInHierarchy)
+        {
             monstro.SetActive(false);
+            obstaculo1.SetActive(false);
+            obstaculo2.SetActive(false);
+            obstaculo3.SetActive(false);
+        }
+           
 
         Destroy(gameObject);
     }
