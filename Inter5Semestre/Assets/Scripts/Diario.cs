0fae3323181efcabff400a36e20bc67c559fd497
Vini Shoji <viniciusasato@gmail.com>
2021-04-30 19:27:40 -0300

    DiaryObjectives

--- Inter5Semestre/Assets/Scripts/Diario.cs
+++ Inter5Semestre/Assets/Scripts/Diario.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Diario : MonoBehaviour
 {
     InputManager inputManager;
 
     private bool firstPage = true;
+
+    [SerializeField]
+    GameObject objectivesPage;
+
+    [SerializeField]
+    private TMP_Text[] objetivosSecundarios;
+    private int objetivoSecundarioIndex;
 
     [SerializeField]
     private GameObject newPageSet;
@@ -25,7 +33,7 @@ public class Diario : MonoBehaviour
 
     [SerializeField]
     private GameObject lastUpdate, lastUpdate2;
-   
+
 
     [SerializeField]
     private Transform leftPosition, rightPosition;
@@ -33,19 +41,87 @@ public class Diario : MonoBehaviour
 
 
     private void Start()
-    {        
+    {
         inputManager = InputManager.Instance;
         currentPage = 0;
+        pages.Add(objectivesPage);
+        bothPagesFilled = true;
+        
     }
 
     private void Update()
     {
+
+    }
+
+    public void AddPrimaryObjective()
+    {
+
+    }
+    public void AddSecondaryObjective(string nomeObjetivo)
+    {
+        objetivosSecundarios[objetivoSecundarioIndex].text = nomeObjetivo;
+        objetivoSecundarioIndex += 1;
         
     }
 
+    public void ConcludeSecondaryObjective(string nomeObjetivo)
+    {
+        for (int i = 0; i < objetivosSecundarios.Length; i++)
+        {
+            if(nomeObjetivo == objetivosSecundarios[i].text)
+            {
+                objetivosSecundarios[i].fontStyle = FontStyles.Strikethrough;
+                return;
+            }
+        }
+    }
+
+
+
+    public void FillPage(GameObject atualizacao)
+    {
+        if (bothPagesFilled)
+        {
+            pages[currentPage].SetActive(false);
+            currentPage += 1;
+            bothPagesFilled = false;
+            leftPageFilled = false;
+            firstPage = false;
+
+
+        }
+
+        if (!leftPageFilled)
+        {
+            newPageSet = new GameObject("Page" + currentPage);
+            newPageSet.transform.parent = this.gameObject.transform;
+            pages.Add(newPageSet);
+            atualizacao.transform.position = leftPosition.position;
+            atualizacao.transform.parent = newPageSet.transform;
+            leftPageFilled = true;
+
+        }
+        else
+        {
+            atualizacao.transform.parent = newPageSet.transform;
+            atualizacao.transform.position = rightPosition.position;
+            bothPagesFilled = true;
+        }
+        if (lastUpdate != null) lastUpdate2 = lastUpdate;
+
+        lastUpdate = atualizacao;
+
+    }
+
+
+
+
+
+
     public void NextPage()
     {
-        if(currentPage+1 >= pages.Count)
+        if (currentPage + 1 >= pages.Count)
         {
             return;
         }
@@ -62,47 +138,11 @@ public class Diario : MonoBehaviour
 
     public void PreviousPage()
     {
-        if(currentPage >= 0)
+        if (currentPage >= 0)
         {
             pages[currentPage].SetActive(false);
             pages[currentPage - 1].SetActive(true);
             currentPage -= 1;
         }
-    }
-
-
-    public void FillPage(GameObject atualizacao)
-    {
-        if (bothPagesFilled)
-        {                     
-                pages[currentPage].SetActive(false);
-                currentPage += 1;
-                bothPagesFilled = false;
-                leftPageFilled = false;
-                firstPage = false;                          
-            
-
-        }
-
-        if (!leftPageFilled)
-        {
-            newPageSet = new GameObject("Page" + currentPage);
-            newPageSet.transform.parent = this.gameObject.transform;
-            pages.Add(newPageSet);
-            atualizacao.transform.position = leftPosition.position;
-            atualizacao.transform.parent = newPageSet.transform;
-            leftPageFilled = true;
-            
-        }
-        else
-        {
-            atualizacao.transform.parent = newPageSet.transform;
-            atualizacao.transform.position = rightPosition.position;
-            bothPagesFilled = true;
-        }
-        if(lastUpdate!= null) lastUpdate2 = lastUpdate;
-       
-        lastUpdate = atualizacao;
-              
     }
 }
