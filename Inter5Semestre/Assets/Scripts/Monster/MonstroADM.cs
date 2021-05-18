43407403495c6ff1ae7b2aee9f94c2f6078ca989
EvandroBustamante <evandro.bustamante@gmail.com>
2021-05-17 21:23:10 -0300

    Stress

--- Inter5Semestre/Assets/Scripts/Monster/MonstroADM.cs
+++ Inter5Semestre/Assets/Scripts/Monster/MonstroADM.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MonstroADM : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class MonstroADM : MonoBehaviour
     [SerializeField]
     private float distanceToDie;
     private float distance;
+    [SerializeField]
+    private Volume stress;
+    private float weightValue;
 
     private void Start()
     {
@@ -19,9 +23,29 @@ public class MonstroADM : MonoBehaviour
     private void Update()
     {
         distance = Vector3.Distance(transform.position, player.position);
-        if(distance <= distanceToDie)
+        
+        if (distance <= distanceToDie + 0.2f) weightValue = 1;
+        else if (distance <= 2.5f) weightValue = 0.75f;
+        else if (distance <= 5) weightValue = 0.5f;
+        else if (distance <= 7.5f) weightValue = 0.25f;
+        else if (distance <= 10) weightValue = 0;
+
+        if(weightValue < stress.weight)
+        {
+            stress.weight -= 0.1f * Time.deltaTime;
+            if (stress.weight < weightValue) stress.weight = weightValue;
+        }
+        if (weightValue > stress.weight)
+        {
+            stress.weight += 0.1f * Time.deltaTime;
+            if (stress.weight > weightValue) stress.weight = weightValue;
+        }
+
+        if (distance <= distanceToDie)
         {
             respawnManager.Respawn1();
         }
+
+        Debug.Log(distance);
     }
 }
